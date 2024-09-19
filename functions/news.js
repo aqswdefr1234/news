8dd7ff762ec01acb07e6267c82e02909493e2035
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2024-09-19 12:41:23 +0900

    Update news.js

--- functions/news.js
+++ functions/news.js
@@ -3,7 +3,8 @@ const xml2js = require('xml2js');
 const { parseStringPromise } = xml2js;
 
 async function getNews(keyword) {
-  const rssUrl = `https://news.google.com/rss/search?q=${keyword}&hl=ko&gl=KR&ceid=KR:ko`;
+  const encodedKeyword = encodeURIComponent(keyword); // 키워드를 URL 인코딩
+  const rssUrl = `https://news.google.com/rss/search?q=${encodedKeyword}&hl=ko&gl=KR&ceid=KR:ko`;
   
   try {
     const response = await axios.get(rssUrl);
@@ -25,10 +26,10 @@ async function getNews(keyword) {
       body: JSON.stringify(newsData),
     };
   } catch (error) {
-    console.error('Error fetching news:', error); // 콘솔에 에러 메시지 출력
+    console.error('Error fetching news:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: `Error fetching news: ${error.message}` }),  // JSON 형식으로 에러 반환
+      body: JSON.stringify({ message: `Error fetching news: ${error.message}` }),
     };
   }
 }
